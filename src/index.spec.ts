1e8eb336217fbf2405564c10f2fda779d184baac
Zbigniew 'zibi' Jarosik <zibi@nora.pl>
2022-02-15 11:29:42 +0100

    added Date.now*() tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,24 +2,33 @@ import 'mocha';
 import {expect} from 'chai';
 import './index';
 
-describe('String extension functions', () => {
+const now = Date.now;
+const timestamp = 1644568675402;
+
+describe('Date get*() functions', () => {
     before(() => {
+        Date.now = () => timestamp;
     });
-    describe('Date().getyyyyMMdd()', () => {
+    after(() => {
+        Date.now = now;
+    });
+    describe('(new Date()).getyyyyMMddHHmmssSSS()', () => {
         const testList = {
-            1644568675402: '2022-02-11',
+            [timestamp]: '2022-02-11 09:37:55.402',
         };
+
         Object.entries(testList).forEach(([src, dst]) =>
             it(`should convert ${src} to ${dst}`, () => {
                 const date = new Date(parseInt(src, 10));
-                expect(date.getyyyyMMdd()).to.be.a('string');
-                expect(date.getyyyyMMdd()).to.be.equal(dst);
+
+                expect(date.getyyyyMMddHHmmssSSS()).to.be.a('string');
+                expect(date.getyyyyMMddHHmmssSSS()).to.be.equal(dst);
             })
         );
     });
-    describe('Date().getyyyyMMddHHmmss()', () => {
+    describe('(new Date()).getyyyyMMddHHmmss()', () => {
         const testList = {
-            1644568675402: '2022-02-11 09:37:55',
+            [timestamp]: '2022-02-11 09:37:55',
         };
         Object.entries(testList).forEach(([src, dst]) =>
             it(`should convert ${src} to ${dst}`, () => {
@@ -30,22 +39,34 @@ describe('String extension functions', () => {
             })
         );
     });
-    describe('Date().getyyyyMMddHHmmssSSS()', () => {
+    describe('(new Date()).getyyyyMMdd()', () => {
         const testList = {
-            1644568675402: '2022-02-11 09:37:55.402',
+            [timestamp]: '2022-02-11',
+        };
+        Object.entries(testList).forEach(([src, dst]) =>
+            it(`should convert ${src} to ${dst}`, () => {
+                const date = new Date(parseInt(src, 10));
+                expect(date.getyyyyMMdd()).to.be.a('string');
+                expect(date.getyyyyMMdd()).to.be.equal(dst);
+            })
+        );
+    });
+    describe('(new Date()).getHHmmssSSS()', () => {
+        const testList = {
+            [timestamp]: '09:37:55.402',
         };
         Object.entries(testList).forEach(([src, dst]) =>
             it(`should convert ${src} to ${dst}`, () => {
                 const date = new Date(parseInt(src, 10));
 
-                expect(date.getyyyyMMddHHmmssSSS()).to.be.a('string');
-                expect(date.getyyyyMMddHHmmssSSS()).to.be.equal(dst);
+                expect(date.getHHmmssSSS()).to.be.a('string');
+                expect(date.getHHmmssSSS()).to.be.equal(dst);
             })
         );
     });
-    describe('Date().getHHmmss()', () => {
+    describe('(new Date()).getHHmmss()', () => {
         const testList = {
-            1644568675402: '09:37:55',
+            [timestamp]: '09:37:55',
         };
         Object.entries(testList).forEach(([src, dst]) =>
             it(`should convert ${src} to ${dst}`, () => {
@@ -56,9 +77,9 @@ describe('String extension functions', () => {
             })
         );
     });
-    describe('Date().getHHmm()', () => {
+    describe('(new Date()).getHHmm()', () => {
         const testList = {
-            1644568675402: '09:37',
+            [timestamp]: '09:37',
         };
         Object.entries(testList).forEach(([src, dst]) =>
             it(`should convert ${src} to ${dst}`, () => {
@@ -69,16 +90,72 @@ describe('String extension functions', () => {
             })
         );
     });
-    describe('Date().getHHmmssSSS()', () => {
+    describe('Date.nowyyyyMMddHHmmssSSS()', () => {
         const testList = {
-            1644568675402: '09:37:55.402',
+            [timestamp]: '2022-02-11 09:37:55.402',
         };
         Object.entries(testList).forEach(([src, dst]) =>
             it(`should convert ${src} to ${dst}`, () => {
-                const date = new Date(parseInt(src, 10));
+                // const date = new Date(parseInt(src, 10));
 
-                expect(date.getHHmmssSSS()).to.be.a('string');
-                expect(date.getHHmmssSSS()).to.be.equal(dst);
+                expect(Date.nowyyyyMMddHHmmssSSS()).to.be.a('string');
+                expect(Date.nowyyyyMMddHHmmssSSS()).to.be.equal(dst);
+            })
+        );
+    });
+    describe('Date.nowyyyyMMddHHmmss()', () => {
+        const testList = {
+            [timestamp]: '2022-02-11 09:37:55',
+        };
+        Object.entries(testList).forEach(([src, dst]) =>
+            it(`should convert ${src} to ${dst}`, () => {
+                expect(Date.nowyyyyMMddHHmmss()).to.be.a('string');
+                expect(Date.nowyyyyMMddHHmmss()).to.be.equal(dst);
+            })
+        );
+    });
+
+    describe('Date.nowyyyyMMdd()', () => {
+        const testList = {
+            [timestamp]: '2022-02-11',
+        };
+        Object.entries(testList).forEach(([src, dst]) =>
+            it(`should convert ${src} to ${dst}`, () => {
+                expect(Date.nowyyyyMMdd()).to.be.a('string');
+                expect(Date.nowyyyyMMdd()).to.be.equal(dst);
+            })
+        );
+    });
+    describe('Date.nowHHmmssSSS()', () => {
+        const testList = {
+            [timestamp]: '09:37:55.402',
+        };
+        Object.entries(testList).forEach(([src, dst]) =>
+            it(`should convert ${src} to ${dst}`, () => {
+                expect(Date.nowHHmmssSSS()).to.be.a('string');
+                expect(Date.nowHHmmssSSS()).to.be.equal(dst);
+            })
+        );
+    });
+    describe('Date.nowHHmmss()', () => {
+        const testList = {
+            [timestamp]: '09:37:55',
+        };
+        Object.entries(testList).forEach(([src, dst]) =>
+            it(`should convert ${src} to ${dst}`, () => {
+                expect(Date.nowHHmmss()).to.be.a('string');
+                expect(Date.nowHHmmss()).to.be.equal(dst);
+            })
+        );
+    });
+    describe('Date.nowHHmm()', () => {
+        const testList = {
+            [timestamp]: '09:37',
+        };
+        Object.entries(testList).forEach(([src, dst]) =>
+            it(`should convert ${src} to ${dst}`, () => {
+                expect(Date.nowHHmm()).to.be.a('string');
+                expect(Date.nowHHmm()).to.be.equal(dst);
             })
         );
     });
